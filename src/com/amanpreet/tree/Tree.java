c3fd0b1230c051c45b45d1f15635869bae0c517e
kauramanp <33446281+kauramanp@users.noreply.github.com>
2022-11-09 00:21:49 +0530

    update tree

--- src/com/amanpreet/tree/Tree.java
+++ src/com/amanpreet/tree/Tree.java
@@ -1,5 +1,29 @@
 package com.amanpreet.tree;
 
 public class Tree {
+	
+	private TNode insert(TNode root, int data) {
+		if(root == null) {
+			return new TNode(data);
+		}
+		
+		if(data <= root.getData()) {
+			root.setLeft(insert(root.getLeft(), data));
+		}else {
+			root.setRight(insert(root.getLeft(), data));
+
+		}
+		
+		return root;
+	}
+	
+	public boolean pairSum(TNode root, int target, Set<Integer> dataSet) {
+		if(root == null) return false;
+		int setVal = target - root.getData();
+		if(dataSet.contains(setVal)) {
+			System.out.print("Pair is : "+ root.getData()+" , "+ setVal);
+			return true;
+		}
+	}
 
 }
